84bb4dc68d6f6bf816f13c3eb22014dd097c63de
ajay1388 <56335475+ajay1388@users.noreply.github.com>
2020-07-22 20:53:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv("creditcard.csv")
+
+print(data.head())
+print(data.info())
+
+X = data.drop(['Class'],axis = 1)
+Y = data['Class']
+
+print(X.shape)
+print(Y.shape)
+xData = X.values
+yData = Y.values
+
+x_train,x_test,y_train,y_test = train_test_split(xData,yData,test_size = 0.2, random_state=42)
+
+rf = RandomForestClassifier()
+rf.fit(x_train,y_train)
+
+y_pred = rf.predict(x_test)
+
+acc_rc = accuracy_score(y_test, y_pred)
+print("Accuracy is {}".format(acc_rc))
